3ab72ef981b62994daef63af639edc8ea877187c
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-03-18 00:42:40 +0800

    Time: 7 ms (92.68%), Space: 5.9 MB (75.74%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.cpp
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         if(x < 0 || (x % 10 == 0 && x > 0) ) return false;
-        int temp = 0;
+        int temp;
+        temp = 0;
         while(x > temp)
         {
             temp = temp*10 + x%10;
