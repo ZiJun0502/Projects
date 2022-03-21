449779a61798ac0bb903ff933f580659dce4de29
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-03-21 17:49:43 +0800

    Time: 20 ms (94.32%), Space: 13.3 MB (78.12%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
@@ -9,110 +9,34 @@
  * };
  */
 class Solution {
-public://using min heap
-    vector<ListNode*> arr;
-    int len;
-    void swapNode(ListNode** a, ListNode**b)
-    {
-        ListNode* temp = *a;
-        *a = *b;
-        *b = temp;
-    }
-    void heapify(int i)
-    {
-        if(i*2+1 <= len)
-        {
-            if(i*2+2 <= len)
-            {
-                if(arr[i]->val > arr[i*2+1]->val || arr[i]->val >arr[i*2+2]->val)
-                {
-                    if(arr[i*2+1]->val < arr[i*2+2]->val)
-                    {
-                        swapNode(&arr[i], &arr[i*2+1]);
-                        heapify(i*2+1);
-                    }
-                    else
-                    {
-                        swapNode(&arr[i], &arr[i*2+2]);
-                        heapify(i*2+2);
-                    }
-                }
-            }
-            if(arr[i]->val > arr[i*2+1]->val)
-            {
-                swapNode(&arr[i], &arr[i*2+1]);
-                return;
-            }
-            heapify(i*2+1);
+public:
+    struct comp{
+        bool operator()(const ListNode* a, const ListNode* b) {
+          return a->val > b->val;
         }
-        return;
-    }
-    void insert(ListNode* node)
-    {
-        arr.push_back(node);
-        // for(auto i : arr)
-        // {
-        //     cout << i->val <<' ';
-        // }
-        // cout <<'\n';
-        heapify_b(arr.size()-1);
-        
-    }
-    void heapify_b(int i)//bottom up heapify for insertion
-    {
-        if(i == 0)
-        {
-            return;
-        }
-        else
-        {
-            if(arr[i]->val < arr[(i-1)/2]->val)
-            {
-                swapNode(&arr[i], &arr[(i-1)/2]);
-                heapify_b((i-1)/2);
-            }
-        }
-    }
-    ListNode* makeNode(int val)
-    {
-        ListNode* node = new ListNode(val);
-        return node;
-    }
+    };
     ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if(lists.size() == 0) return NULL;
+        priority_queue<ListNode*, vector<ListNode*>, comp> pq;
         for(auto i : lists)
+            if (i != NULL)
+                pq.push(i);
+        if(pq.empty()) return NULL;
+        ListNode* head = new ListNode(pq.top()->val);
+        ListNode* iter = head; 
+        ListNode* temp= pq.top()->next;
+        pq.pop();
+        if (temp != NULL)
+        pq.push(temp);
+        
+        while(pq.empty() != 1)
         {
-            if(i != NULL)
-            insert(i);
-        }
-        len = arr.size()-1;
-        if(len < 0 || arr[0] == NULL) return NULL;
-        if(len == 0) return arr[0];
-        ListNode* head = makeNode(arr[0]->val);
-        ListNode* iter = head;
-        ListNode* node;
-        if(arr[0]->next == NULL)
-            {
-                if(len > 0)
-                swapNode(&arr[0],&arr[len--]);
-            }
-        else
-            arr[0] = arr[0]->next;
-        heapify(0);
-        for(;;)
-        {
-            node = makeNode(arr[0]->val);
-            //cout<<node->val<<' ';
-            iter->next = node;
+            temp = pq.top();
+            iter->next = temp;
             iter = iter->next;
-            if(arr[0]->next == NULL)
-            {
-                if(len > 0)
-                swapNode(&arr[0],&arr[len--]);
-                else break;
-            }
-            else
-                arr[0] = arr[0]->next;
-            heapify(0);
+            pq.pop();
+            if(temp->next != NULL)
+            pq.push(temp->next);
         }
         return head;
     }
