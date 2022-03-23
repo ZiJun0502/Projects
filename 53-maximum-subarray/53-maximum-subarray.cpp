cda43c0ef5dbefe15627a56906e024dd1766fda1
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-03-23 16:17:15 +0800

    Time: 150 ms (49.90%), Space: 67.8 MB (11.95%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int ans = -21478;
+        int ans = -10000;
         int temp = 0;
         for(int i = 0 ; i < nums.size() ; i++)
         {
