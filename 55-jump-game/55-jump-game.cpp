3ac0443a869a132099405e5f75fc32821671ac51
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-03-26 20:35:06 +0800

    Time: 84 ms (65.65%), Space: 48.2 MB (91.59%) - LeetHub

---
+++ 55-jump-game/55-jump-game.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size()-1;
+        if(n == 0) return true;
+        int far = 0;
+        int cur = 0;
+        for(int i = 0 ; i < n ; i++)
+        {
+            far = max(far, i + nums[i]);
+            if(i == far)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
