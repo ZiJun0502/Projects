a19571e90695f7d7d5bc7a4b0174fc825234b8cc
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-03-26 20:35:24 +0800

    Time: 64 ms (86.63%), Space: 48.3 MB (91.59%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -4,7 +4,6 @@ public:
         int n = nums.size()-1;
         if(n == 0) return true;
         int far = 0;
-        int cur = 0;
         for(int i = 0 ; i < n ; i++)
         {
             far = max(far, i + nums[i]);
