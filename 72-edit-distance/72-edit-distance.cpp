88d72ef82e3e98cb7c622f0b859c786d1da76a1e
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-03-28 17:12:12 +0800

    Time: 4 ms (97.26%), Space: 6.1 MB (99.62%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -7,8 +7,8 @@ public:
         int dp[b + 1];// dp[i][j] denote the number of operations to convert word1[0:i] to word2[0:j]
         for(int i = 0 ; i <= b ; i++) dp[i] = i;
         int i0 = 1; // first element in i-th row
-        int temp = dp[0]; // record the dp[i-1][j-1] element
-        int tempp = 0;
+        int temp; // record the dp[i-1][j-1] element
+        int tempp;
         for(int i = 1 ; i <= a ; i++)
         {
             //for(auto i : dp) cout << i <<' '; cout <<'\n';  
