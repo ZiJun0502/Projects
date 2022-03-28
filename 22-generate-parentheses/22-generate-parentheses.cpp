5821aa101cceea4b4bc4001e52cfe2b0240900b3
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-03-29 01:58:18 +0800

    Time: 13 ms (17.11%), Space: 14 MB (31.89%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -2,19 +2,19 @@ class Solution {
 public:
     vector<string> ans;
     int n0;
-    void create(int count, string s, int l, int r)
+    void create(string s, int l, int r)
     {
-        if(count < 2 * n0)
+        if(l + r < 2 * n0)
         {
-            if(l < n0) create(count + 1, s + '(', l + 1, r);
-            if(l > r) create(count + 1, s + ')', l , r + 1);
+            if(l < n0) create(s + '(', l + 1, r);
+            if(l > r) create(s + ')', l , r + 1);
         }
         else
             ans.push_back(s);
     }
     vector<string> generateParenthesis(int n) {
         n0 = n;
-        create(0, "", 0, 0);
+        create("", 0, 0);
         return ans;
     }
 };
