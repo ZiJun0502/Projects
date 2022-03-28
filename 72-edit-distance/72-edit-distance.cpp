5336bae01e8fe0c69e3a122ae0b34a2aaffe4640
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-03-28 17:11:50 +0800

    Time: 8 ms (89.18%), Space: 6.1 MB (99.62%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,20 +1,37 @@
 class Solution {
 public:
-    int minDistance(string word1, string word2) 
+    int minDistance(string word2, string word1) 
     {
         int a = word1.size();
         int b = word2.size();
-        int dp[a + 1][b + 1];// dp[i][j] denote the number of operations to convert word1[0:i] to word2[0:j]
-        for(int i = 0 ; i <= a ; i++)
+        int dp[b + 1];// dp[i][j] denote the number of operations to convert word1[0:i] to word2[0:j]
+        for(int i = 0 ; i <= b ; i++) dp[i] = i;
+        int i0 = 1; // first element in i-th row
+        int temp = dp[0]; // record the dp[i-1][j-1] element
+        int tempp = 0;
+        for(int i = 1 ; i <= a ; i++)
         {
+            //for(auto i : dp) cout << i <<' '; cout <<'\n';  
+
             for(int j = 0 ; j <= b ; j++)
             {
-                if(i==0) dp[i][j] = j;
-                else if(j == 0) dp[i][j] = i;
-                else if(word1[i-1] == word2[j-1]) dp[i][j] = dp[i-1][j-1];
-                else dp[i][j] = 1 + min(dp[i-1][j], min(dp[i][j-1], dp[i-1][j-1]));
+                if(j != 0)
+                {
+                    tempp = dp[j];
+                    if(word1[i-1] == word2[j-1]) dp[j] = temp;
+                    else dp[j] = 1 + min(temp, min(dp[j], dp[j - 1]));
+                    temp = tempp;
+                }
+                else
+                {
+                    temp = dp[j];
+                    dp[j] = i;
+                }
             }
+            //for(auto i : dp) cout << i <<' '; cout <<'\n';  
+
         }
-        return dp[a][b];
+        
+        return dp[b];
     }
 };
