43e5fd9bc248c7bbf5247f32ba7c6b74b7262b1a
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-03-30 00:38:14 +0800

    Time: 4 ms (23.37%), Space: 6.6 MB (59.80%) - LeetHub

---
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    map<char, string> m;
+    string s;
+    int len;
+    vector<string> ans;
+    string temp;
+    void solver(int count = 0)
+    {
+        if(count < len)
+        {
+            for(auto i : m[temp[count]])
+            {
+                s.push_back(i);
+                solver(count + 1);
+                s.pop_back();
+            }
+        }
+        else if(count == len ) ans.push_back(s);
+    }
+    vector<string> letterCombinations(string digits) {
+        len = digits.size();
+        if (len == 0) return ans;
+        temp = digits;
+        m['2'] = "abc";
+        m['3'] = "def";
+        m['4'] = "ghi";
+        m['5'] = "jkl";
+        m['6'] = "mno";
+        m['7'] = "pqrs";
+        m['8'] = "tuv";
+        m['9'] = "wxyz";
+        solver();
+        return ans;
+    }
+};
