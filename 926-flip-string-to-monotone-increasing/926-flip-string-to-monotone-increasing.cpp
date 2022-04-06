627f0847906f98224cc21f2979380d974fba4d28
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-04-06 22:27:17 +0800

    Time: 23 ms (94.86%), Space: 11.1 MB (53.90%) - LeetHub

--- 926-flip-string-to-monotone-increasing/926-flip-string-to-monotone-increasing.cpp
+++ 926-flip-string-to-monotone-increasing/926-flip-string-to-monotone-increasing.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int minFlipsMonoIncr(string s) {
-        int count1 = 0, countFlips = 0;
+        int countOnes = 0, countFlips = 0;
         for (int i = 0 ; i < s.size() ; i++) 
         {
             if (s[i] == '1') 
-                ++count1;
+                ++countOnes;
             else 
                 ++countFlips;
-            countFlips = min(count1, countFlips);
+            countFlips = min(countOnes, countFlips);
         }
         return countFlips;
     }
