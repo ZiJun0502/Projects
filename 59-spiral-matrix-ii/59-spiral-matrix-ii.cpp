fbeea49dbfa3a6094b0213057601168c60bcb8f8
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-04-12 00:41:49 +0800

    Time: 3 ms (33.71%), Space: 6.5 MB (63.69%) - LeetHub

---
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        vector<vector<int>>ans(n, vector<int>(n));
+        int size = n * n;
+        int x = -1, y = 0, dx = 1, dy = 0;
+        int mini = 0, maxi = n - 1;
+        
+        for(int k = 1 ; k <= size ; k++)
+        {
+            y += dy, x += dx;
+            ans[y][x] = k;
+            if(dx == 1 && x == maxi)
+            {
+                dx = 0;
+                dy = 1;
+            }
+            else if(dy == 1 && y == maxi)
+            {
+                dy = 0;
+                dx = -1;
+            }
+            else if(dx == -1 && x == mini)
+            {
+                maxi--;
+                mini++;
+                dx = 0;
+                dy = -1;
+            }
+            else if(dy == -1 && y == mini)
+            {
+
+                dy = 0;
+                dx = 1;
+            }
+        }
+        return ans;
+    }
+};
