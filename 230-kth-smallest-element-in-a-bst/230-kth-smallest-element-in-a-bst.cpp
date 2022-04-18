2d9d668c0f8e22ca839288d26cce62acd54ae11a
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-04-19 03:37:30 +0800

    Time: 15 ms (92.83%), Space: 24.1 MB (61.00%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
@@ -16,8 +16,7 @@ public:
         if(root){
             kthSmallest(root->left,k);
             k--;
-            if(k==0) 
-            {ans=root->val;}
+            if(k==0) ans=root->val;
             kthSmallest(root->right,k);
         }
         return ans;
