a5098473c2f443496b2a1dbd218969e0a1f4322f
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-04-25 13:47:10 +0800

    Time: 0 ms (100.00%), Space: 6 MB (56.10%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -2,11 +2,15 @@ class Solution {
 public:
     int climbStairs(int n) {
         int dp[n+1];
-        dp[0] = dp[1] = 1;
-        for(int i = 2 ; i <= n ; i++)
+        int ans = 1;
+        int a0 = 1;
+        int a1 = 1;
+        for(int i = 0 ; i < n-1 ; i++)
         {
-            dp[i] = dp[i-1] + dp[i-2];
+            ans = a0 + a1;
+            a0 = a1;
+            a1 = ans;
         }
-        return dp[n];
+        return ans;
     }
 };
