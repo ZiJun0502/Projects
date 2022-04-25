7ac23d571408d9fdefcbc8cf9bf61f328445aaf6
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-04-25 13:49:01 +0800

    Time: 0 ms (100.00%), Space: 6 MB (56.10%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        int ans = 1;
-        int a0 = 1;
-        int a1 = 1;
+        int ans,a0,a1;
+        ans = a0 = a1 = 1;
         for(int i = 0 ; i < n-1 ; i++)
         {
             ans = a0 + a1;
