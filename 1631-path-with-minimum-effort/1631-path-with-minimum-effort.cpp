72d9367ff32a07960a7a7df88d802ebb4a5f58bc
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-04-29 04:06:04 +0800

    Time: 178 ms (84.75%), Space: 20.1 MB (77.83%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -4,52 +4,43 @@ class Solution {
         {0,-1},         {0,1},
                 {1, 0}
     };
-    bool isValid(vector<vector<int>>& heights, int mx) {
-        queue<pair<int,int>> q;
-        int ROWS = heights.size();
-        int COLS = heights[0].size();
-        
-        vector<vector<bool>> seen(ROWS, vector<bool>(COLS, false));
-        q.push({0,0});
-        seen[0][0] = true;
-        while(!q.empty()) {
-            auto node = q.front();
-            q.pop();
-            int r = node.first;
-            int c = node.second;
-            if(r == ROWS - 1 && c == COLS - 1) return true;
-            for(auto& dir : dirs) {
-                int dr = dir[0] + r;
-                int dc = dir[1] + c;
-                if(dr < 0 || dr >= ROWS) continue;
-                if(dc < 0 || dc >= COLS) continue;
-                if(seen[dr][dc]) continue;
-                if(abs(heights[r][c] - heights[dr][dc]) > mx) continue;
-                seen[dr][dc] = true;
-                q.push({dr, dc});
-            }
-        }
-        
-        return false;
-    }
     
 public:
     int minimumEffortPath(vector<vector<int>>& heights) {
-
-        int L = 0;
-        int R = INT_MAX;
-        int best = INT_MAX;
-        while(L <= R) {
-            int M = L + (R-L) / 2;
-            
-            if(isValid(heights, M)) {
-                R = M - 1;
-                best = min(best, M);
-            } else {
-                L = M + 1;
-            }
-        }
+        using pipii = pair<int,pair<int,int>>;
+        priority_queue<pipii, vector<pipii>, greater<pipii>> pq;
         
-        return best;
+        int ROWS = heights.size();
+        int COLS = heights[0].size();
+        
+        vector<vector<int>> dists(ROWS, vector<int>(COLS, INT_MAX));
+        
+        dists[0][0] = 0;
+        
+        pq.push({0, {0,0}});
+        while(!pq.empty()) {
+            auto node = pq.top();
+            pq.pop();
+            
+            int weight = node.first;
+            auto coords = node.second;
+            int r = coords.first;
+            int c = coords.second;
+            if(dists[r][c] < weight) continue;
+            if(r == ROWS - 1 && c == COLS - 1) return weight;
+            
+            for(auto& dir : dirs) {
+                int dr = r + dir[0];
+                int dc = c + dir[1];
+                if(dr < 0 || dr >= ROWS) continue;
+                if(dc < 0 || dc >= COLS) continue;
+                int newWeight = abs(heights[r][c] - heights[dr][dc]);
+                newWeight = max(newWeight, weight);
+                if(dists[dr][dc] <= newWeight) continue;
+                dists[dr][dc] = newWeight;
+                pq.push({newWeight, {dr, dc}});         
+            }  
+        }     
+        return 0;      
     }
 };
