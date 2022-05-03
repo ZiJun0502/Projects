7c0a97e8ccfccf8ffce10b306074138f46732aeb
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-05-03 21:59:18 +0800

    Time: 57 ms (22.67%), Space: 26.4 MB (98.60%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
@@ -1,14 +1,37 @@
 class Solution {
 public:
     int findUnsortedSubarray(vector<int>& nums) {
-        vector<int> arr = nums;
-        sort(arr.begin(), arr.end());
-        int len = 0;
-        int l,r = nums.size() - 1;
-        for(l = 0 ; nums[l] == arr[l] && l < r ; l++)
-        {}
-        for(r ; nums[r] == arr[r] && r > l ; r--)
-        {}
-        return r - l == 0 ? 0 : r - l + 1;
+        int n = nums.size();
+        int left = -1;
+        int right = -1;
+        //prefix sub sorted array
+        int i;
+        for(i = 0 ; i < n-1; i++)
+        {
+            if(nums[i] > nums[i+1])
+            {
+                left = i;
+                break;
+            }  
+        }
+        if(i == n-1) return 0;
+        int mini = *min_element(nums.begin() + left + 1, nums.end());
+        int min = upper_bound(nums.begin(), nums.begin() + left, mini) - nums.begin();
+        //cout << mini <<' ' << min << '\n';
+        for(i = n-1; i > 0; i--)
+        {
+            if(nums[i] < nums[i-1])
+            {
+                right = i;
+                break;
+            }
+            
+        }
+        if(i == 0) return 0;
+        //cout << right <<'\n';
+        int maxi = *max_element(nums.begin(), nums.begin() + right);
+        int max = lower_bound(nums.begin() + right, nums.end(), maxi) - nums.begin();
+        //cout << maxi <<' ' << max << '\n';
+        return max - min;
     }
 };
