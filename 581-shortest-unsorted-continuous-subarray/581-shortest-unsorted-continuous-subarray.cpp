afebfed0d5b8d46a4b7a354715f9fdbdca27c620
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-05-03 21:59:51 +0800

    Time: 58 ms (21.05%), Space: 26.5 MB (68.90%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
@@ -15,9 +15,7 @@ public:
             }  
         }
         if(i == n-1) return 0;
-        int mini = *min_element(nums.begin() + left + 1, nums.end());
-        int min = upper_bound(nums.begin(), nums.begin() + left, mini) - nums.begin();
-        //cout << mini <<' ' << min << '\n';
+        
         for(i = n-1; i > 0; i--)
         {
             if(nums[i] < nums[i-1])
@@ -28,10 +26,11 @@ public:
             
         }
         if(i == 0) return 0;
-        //cout << right <<'\n';
+        int mini = *min_element(nums.begin() + left + 1, nums.end());
+        int min = upper_bound(nums.begin(), nums.begin() + left, mini) - nums.begin();
+        
         int maxi = *max_element(nums.begin(), nums.begin() + right);
         int max = lower_bound(nums.begin() + right, nums.end(), maxi) - nums.begin();
-        //cout << maxi <<' ' << max << '\n';
         return max - min;
     }
 };
