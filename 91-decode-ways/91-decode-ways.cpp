6dcd329d483f52b743bfe2c5afc0815a066e2e6f
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-05-06 22:55:11 +0800

    Time: 4 ms (39.81%), Space: 6.1 MB (72.71%) - LeetHub

---
+++ 91-decode-ways/91-decode-ways.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        if(s[0] =='0') return 0;
+        int n = s.size();
+        int dp[n+1];
+        dp[0] = 1;
+        if(s[1] == '0')
+        {
+            dp[1] = 0;
+        }
+        else dp[1] = 1;
+        for(int i = 2 ; i <= n ; i++)
+        {
+            dp[i] = 0;
+            if(s[i-1] != '0')
+            {
+                // take one number
+                dp[i] += dp[i - 1];
+            }
+            if(s[i-2] =='1' || (s[i-2] =='2' && s[i-1] <= '6' && s[i-1] >= '0'))
+            {
+                dp[i] += dp[i - 2];
+            }
+        }
+        return dp[n];
+    }
+};
