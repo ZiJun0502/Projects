9a6c52aa608b9f9a90cccaeec07b8d9bab42f3a7
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-05-09 19:08:22 +0800

    Time: 0 ms (100.00%), Space: 6.6 MB (33.81%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -4,12 +4,12 @@ public:
         vector<vector<int>> ans(numRows, {1});
         for(int i = 1 ; i < numRows ; i++)
         {
-            ans[i].resize(i);
+            ans[i].resize(i+1);
             for(int j = 1 ; j < i ; j++)
             {
                 ans[i][j] = ans[i - 1][j - 1] + ans[i - 1][j];
             }
-            ans[i].push_back(1);
+            ans[i][i] = 1;
         }
         return ans;
     }
