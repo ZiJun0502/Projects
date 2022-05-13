bc5ee75c7623407863553142ec697f62aa953ce8
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-05-14 01:09:14 +0800

    Time: 0 ms (100.00%), Space: 8.6 MB (92.85%) - LeetHub

--- 27-remove-element/27-remove-element.cpp
+++ 27-remove-element/27-remove-element.cpp
@@ -6,7 +6,7 @@ public:
         {
             while(nums[i] == val)
             {
-                nums[i] = -2;
+                nums[i] = -1;
                 swap(nums[i], nums[count--]);
             }
         }
