dcaeda527724c8654199b7a409460523c09cbab5
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-05-15 10:51:51 +0800

    Time: 0 ms (100.00%), Space: 6 MB (31.10%) - LeetHub

--- 29-divide-two-integers/29-divide-two-integers.cpp
+++ 29-divide-two-integers/29-divide-two-integers.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int divide(int dividend, int divisor) {
         if(dividend==INT_MIN && divisor == -1){
-            return 2147483647;
+            return INT_MAX;
         }
         return dividend / divisor;
     }
