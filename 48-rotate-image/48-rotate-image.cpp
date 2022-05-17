0eafdef8bf31a48ee8dffe94bf7bb03a210db04f
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-05-17 19:06:40 +0800

    Time: 3 ms (59.13%), Space: 6.9 MB (92.80%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -7,10 +7,10 @@ public:
     };
     void rotate(vector<vector<int>>& m) {
         pt pos1, pos2, pos3, pos4;
-        int n = m.size();
+        int n = m.size()-1;
         //diagonal length / 2
-        int size = n / 2;
-        int len = n-1;
+        int size = (n+1) / 2;
+        int len = n;
         // record starting value for swapping
         for(int i = 0 ; i < size ; i++)
         {
@@ -19,10 +19,10 @@ public:
                 pos1.i = i;
                 pos1.j = i + j;
                 pos2.i = i + j;
-                pos2.j = n - i - 1;
-                pos3.i = n - i - 1;
-                pos3.j = n - j - 1 - i;
-                pos4.i = n - j - 1 - i;
+                pos2.j = n - i;
+                pos3.i = n - i;
+                pos3.j = n - j - i;
+                pos4.i = n - j - i;
                 pos4.j = i;
                 swap(m[pos4.i][pos4.j], m[pos1.i][pos1.j]);
                 swap(m[pos4.i][pos4.j], m[pos3.i][pos3.j]);
