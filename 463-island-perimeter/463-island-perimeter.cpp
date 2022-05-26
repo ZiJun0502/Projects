f622eaa37e340890c0a1af11f460ab94c3099780
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-05-27 05:43:41 +0800

    Time: 132 ms (71.54%), Space: 96.5 MB (36.92%) - LeetHub

--- 463-island-perimeter/463-island-perimeter.cpp
+++ 463-island-perimeter/463-island-perimeter.cpp
@@ -1,58 +1,56 @@
 class Solution {
 public:
     int ans, n ,m;
-    vector<vector<int>>* g;
-    void dfs(int i, int j)
+    void dfs(int i, int j, vector<vector<int>>& g)
     {
         int count = 0;
         ans += 4;
-        (*g)[i][j] = 2;
+        g[i][j] = 2;
         if(i+1 < n)
         {
-            if((*g)[i + 1][j] == 1)
+            if(g[i + 1][j] == 1)
             {
-                dfs(i + 1, j);
+                dfs(i + 1, j, g);
                 ans--;
             }
-            else if((*g)[i + 1][j] == 2)
+            else if(g[i + 1][j] == 2)
                 ans--;
         }
         if(i-1 >= 0)
         {
-            if((*g)[i - 1][j] == 1)
+            if(g[i - 1][j] == 1)
             {
-                dfs(i - 1, j);
+                dfs(i - 1, j, g);
                 ans--;
             }
-            else if((*g)[i - 1][j] == 2)
+            else if(g[i - 1][j] == 2)
                 ans--;
         }
         //cout << valid(i, j + 1) << '\n' << (*g)[i][j+1] <<'\n';
         if(j+1 < m)
         {
-            if((*g)[i][j + 1] == 1)
+            if(g[i][j + 1] == 1)
             {
-                dfs(i, j + 1);
+                dfs(i, j + 1, g);
                 ans--;
             }
-            else if((*g)[i][j + 1] == 2)
+            else if(g[i][j + 1] == 2)
                 ans--;
         }
         if(j-1 >= 0)
         {
-            if((*g)[i][j - 1] == 1)
+            if(g[i][j - 1] == 1)
             {
-                dfs(i, j - 1);
+                dfs(i, j - 1, g);
                 ans--;
             }
-            else if((*g)[i][j - 1] == 2)
+            else if(g[i][j - 1] == 2)
                 ans--;
         }
     }
     int islandPerimeter(vector<vector<int>>& grid) {
         n = grid.size();
         m = grid[0].size();
-        g = &grid;
         
         for(int i = 0 ; i < n ; i++)
         {
@@ -60,7 +58,7 @@ public:
             {
                 if(grid[i][j] == 1)
                 {
-                    dfs(i, j);
+                    dfs(i, j, grid);
                     goto end;
                 }
             }
