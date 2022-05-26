ef82a85bd07d8f7b1bc70c751809e74bab69750b
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-05-27 04:06:55 +0800

    Time: 14 ms (60.25%), Space: 21.3 MB (40.76%) - LeetHub

--- 112-path-sum/112-path-sum.cpp
+++ 112-path-sum/112-path-sum.cpp
@@ -13,7 +13,8 @@ class Solution {
 public:
     bool hasPathSum(TreeNode* root, int targetSum) {
         if(!root) return false;
-        if(targetSum == root->val && !root->left && !root->right) return true;
-        return hasPathSum(root->left, targetSum - root->val) || hasPathSum(root->right, targetSum - root->val);
+        int val = root->val;
+        if(targetSum == val && !root->left && !root->right) return true;
+        return hasPathSum(root->left, targetSum - val) || hasPathSum(root->right, targetSum - val);
     }
 };
