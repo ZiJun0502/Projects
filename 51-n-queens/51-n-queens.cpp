b1650f2c50af183c1cf5f7fa6671104cc6307bd5
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-06-04 21:14:46 +0800

    Time: 3 ms (96.12%), Space: 7.3 MB (55.22%) - LeetHub

---
+++ 51-n-queens/51-n-queens.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<vector<string>> ans;
+    vector<string> temp;
+    bool col[10], row[10], left[20], right[20];
+    int n;
+    bool valid(int i, int j)
+    {
+        if(col[j] || row[i] || right[i + j] || left[j - i + n]) return false;
+        return true;
+    }
+    void solve(int i)
+    {
+        for(int j = 0 ; j < n ; j++)
+        {
+            if(valid(i, j))
+            {
+                temp[i][j] = 'Q';
+                if(i == n - 1)
+                {
+                    ans.push_back(temp);
+                    temp[i][j] = '.';
+                    return;
+                }
+                col[j] = 1;
+                row[i] = 1;
+                left[j - i + n] = 1;
+                right[i + j] = 1;
+                solve(i + 1);
+                temp[i][j] = '.';
+                col[j] = row[i] = left[j - i + n] = right[i + j] = 0;
+            }
+        }
+    }
+    vector<vector<string>> solveNQueens(int nn) {
+        n = nn;
+        temp = vector<string>(n, string(n, '.'));
+        solve(0);
+        return ans;
+    }
+};
