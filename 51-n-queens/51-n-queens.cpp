d599d1a5b8abc45f50f69c8739e63234de9a6a46
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-06-04 21:17:51 +0800

    Time: 16 ms (35.92%), Space: 7.2 MB (67.79%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -9,7 +9,7 @@ public:
         return !(col[j] || row[i] || right[i + j] || left[j - i + n]);
         
     }
-    void solve(int i = 0)
+    void solve(int i)
     {
         for(int j = 0 ; j < n ; j++)
         {
@@ -35,7 +35,7 @@ public:
     vector<vector<string>> solveNQueens(int nn) {
         n = nn;
         temp = vector<string>(n, string(n, '.'));
-        solve();
+        solve(0);
         return ans;
     }
 };
