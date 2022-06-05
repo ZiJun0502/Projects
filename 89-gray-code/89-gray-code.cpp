f02e18f3a1613cfcc86543909a9a9b54f078b4ef
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-06-06 00:26:07 +0800

    Time: 16 ms (37.75%), Space: 11.9 MB (64.99%) - LeetHub

--- 89-gray-code/89-gray-code.cpp
+++ 89-gray-code/89-gray-code.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     vector<int> grayCode(int n) {
         vector<int> ans;
-        n = 1 << n;
-        for(int i = 0 ; i < n ; i++)
+        int len = 1 << n;
+        for(int i = 0 ; i < len ; i++)
         {
             ans.push_back(i ^ (i/2));
         }
