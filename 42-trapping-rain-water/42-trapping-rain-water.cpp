88738dac0287f919df8a75dd470711d9f7ef2415
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-06-09 20:24:56 +0800

    Time: 16 ms (73.50%), Space: 19.6 MB (42.66%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
     int trap(vector<int>& h) {
-        int r = h.size() - 1, l = 0, lm = 0, rm = 0, ans = 0;
+        int r = h.size() - 1, l = 0, lm = 0, rm = 0;
+        int ans = 0;
         while(r > l)
         {
-            if(h[r] > h[l])
+            int right = h[r], left = h[l];
+            if(right > left)
             {
-                if(h[l] < lm) 
-                    ans += lm - h[l];
+                if(left < lm) 
+                    ans += lm - left;
                 else
-                    lm = h[l];
+                    lm = left;
                 l++;
             }
             else
             {
-                if(h[r] < rm)
-                    ans += rm - h[r];
+                if(right < rm)
+                    ans += rm - right;
                 else
-                    rm = h[r];
+                    rm = right;
                 r--;
             }
         }
