347539c9fe7ae4013aaeb1b9962c301f5a32427c
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-06-12 21:54:40 +0800

    Time: 340 ms (77.77%), Space: 89 MB (99.88%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
@@ -2,12 +2,14 @@ class Solution {
 public:
     int maximumUniqueSubarray(vector<int>& nums) {
         int l = 0, n = nums.size();
-        unordered_map<int ,int> m;
+        //unordered_map<int ,int> m;
+        int m[10001];
+        memset(m, -1, 4*10001);
         int paSum = 0, maxi = 0;
         for(int i = 0 ; i < n ; i++)
         {
             paSum += nums[i];
-            if(m.find(nums[i]) != m.end())
+            if(m[nums[i]] != -1)
             {
                 maxi = max(maxi, paSum - nums[i]);
                 int temp = max(l, m[nums[i]] + 1);
