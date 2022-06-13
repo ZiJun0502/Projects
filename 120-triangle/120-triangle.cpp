d70b1dff35cc7f07d59af4be928ac4befccd673b
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-06-13 09:19:58 +0800

    Time: 9 ms (49.75%), Space: 8.6 MB (70.75%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -18,7 +18,7 @@ public:
         //         cout << j <<' ';
         //     cout << '\n';
         // }
-        int mini = 1000;
+        int mini = 10000;
         n--;
         for(int i = 0 ; i <= n ; i++)
         {
