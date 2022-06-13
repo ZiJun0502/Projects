64be944d7e5a8c375737a0f66d5fa57ff2e57ca0
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-06-13 09:10:14 +0800

    Time: 9 ms (49.75%), Space: 8.9 MB (41.37%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -2,11 +2,25 @@ class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        vector<vector<int>> dp;
+        int **dp;
+        dp = new int*[n];
+        for(int i = 0 ; i < n ; i++)
+            dp[i] = new int[i+1];
         for(int i = 0 ; i < n ; i++)
         {
-            dp.push_back(vector<int>(i+1, 10000));
+            for(int j = 0 ; j <= i ; j++)
+            {
+                //cout << i <<' '<<j<<'\n';
+                dp[i][j] = 10000;
+                //cout << dp[i][j] <<' ';
+            }
+            //cout << '\n';
         }
+        // vector<vector<int>> dp;
+        // for(int i = 0 ; i < n ; i++)
+        // {
+        //     dp.push_back(vector<int>(i+1, 10000));
+        // }
         // for(int i = 0 ; i < n ; i++)
         // {
         //     for(int j : dp[i]) 
