a7c02e860bcc437bba42906a0df7a860be3f8522
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-06-19 18:43:40 +0800

    Time: 32 ms (99.69%), Space: 26.4 MB (81.60%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.cpp
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
-    vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
-        sort(products.begin(), products.end());
+    vector<vector<string>> suggestedProducts(vector<string>& ss, string s) {
+        sort(ss.begin(), ss.end());
         vector<vector<string>> ans;
-        int left = 0, right = products.size() - 1;
-        for (int i = 0; i < searchWord.length(); i++) {
-            vector<string> res;
-            char c = searchWord[i];
-            while (left <= right && (products[left].length() == i || products[left][i] < c)) left++;
-            while (left <= right && (products[right].length() == i || products[right][i] > c)) right--;
-            for (int j = 0; j < 3 && left + j <= right; j++)
-                res.push_back(products[left+j]);
-            ans.push_back(res);
+        int m = s.size();
+        int l = 0, r = ss.size()-1;
+        // we only need to check those strings between l and r.
+        // the other strings are eliminated in the first iteration.
+        for(int i = 0 ; i < m ; i++)
+        {
+            char c = s[i];
+            vector<string> temp;
+            while(l <= r && (i >= ss[l].size() || ss[l][i] < c)) 
+                l++;
+            while(l <= r && (i >= ss[r].size() || ss[r][i] > c)) 
+                r--;
+            for(int j = l ; j <= r && j < l + 3 ; j++)
+                temp.push_back(ss[j]);
+            ans.push_back(temp);
         }
         return ans;
     }
