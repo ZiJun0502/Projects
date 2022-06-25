a3c67d70759ba1067ab81f67409371d68bd7de2b
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-06-26 04:49:33 +0800

    Time: 42 ms (46.84%), Space: 27.1 MB (53.45%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.cpp
+++ 665-non-decreasing-array/665-non-decreasing-array.cpp
@@ -6,9 +6,7 @@ public:
         for(int i = 1; i < n; i++){
             if(nums[i] < nums[i-1]){
                 if(++cnt > 1) return false;
-                if(i == 1 || nums[i-2] <= nums[i])
-                {}
-                else 
+                if(!(i == 1 || nums[i-2] <= nums[i]))
                     nums[i] = nums[i-1];
             }
         }
