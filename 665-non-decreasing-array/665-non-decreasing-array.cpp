7f4988cbd4f6e75c3d08f090b8e90299dcb53491
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-06-26 04:52:21 +0800

    Time: 35 ms (71.10%), Space: 27.1 MB (12.18%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.cpp
+++ 665-non-decreasing-array/665-non-decreasing-array.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
     bool checkPossibility(vector<int>& nums) {
-        bool check = false;             
+        int cnt = 0;              
         int n = nums.size();
         for(int i = 1; i < n; i++){
             if(nums[i] < nums[i-1]){
+                if(++cnt > 1) return false;
                 if(!(i == 1 || nums[i-2] <= nums[i]))
-                {
                     nums[i] = nums[i-1];
-                }
-                if(check)
-                    return false;
-                else
-                    check = true;
             }
         }
         return true;
