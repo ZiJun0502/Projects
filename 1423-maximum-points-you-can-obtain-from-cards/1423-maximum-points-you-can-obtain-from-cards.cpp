5d5b736daf655786c7ae5fb5fcc2c8a2fc20dad9
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-06-26 18:39:49 +0800

    Time: 106 ms (27.96%), Space: 42.4 MB (80.56%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -4,7 +4,7 @@ public:
         int ans = 0;
         int n = v.size()-1;
         int sum = 0;
-        k -= 1;
+        k --;
         for(int i = 0 ; i <= k ; i++)
             sum += v[i];
         ans = sum;
