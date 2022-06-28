e1cab4beeb6cce106531be36d09615eafc521290
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-06-29 04:00:49 +0800

    Time: 92 ms (52.52%), Space: 17.2 MB (95.27%) - LeetHub

---
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minDeletions(string s) {
+        vector<int> freq(26, 0);
+        for(char c : s) 
+            freq[c-'a']++;
+        int ans = 0;
+        sort(freq.begin(), freq.end());
+        for(int i = 24 ; i > -1 ; i--)
+        {
+            if(!freq[i]) break;
+            if(freq[i+1] <= freq[i])
+            {
+                ans+=freq[i] - freq[i+1] + 1;
+                freq[i] = freq[i+1] - 1;
+                if(freq[i+1] == 0)
+                {
+                    ans--;
+                    freq[i]++;
+                }
+            }
+            
+        }
+        return ans;
+    }
+};
