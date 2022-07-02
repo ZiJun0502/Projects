f357f4fd32ced373b62653ba8600620f76af6f8e
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-07-02 19:13:45 +0800

    Time: 0 ms (100.00%), Space: 6 MB (43.02%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        return pow(x, n);
+        unsigned int nn = n;
+        double ans = 1;
+        if(n < 0) nn = -nn;
+        while(nn)
+        {
+            if(nn % 2 == 0)
+            {
+                x *= x;
+                nn /= 2;
+            }
+            else
+            {
+                ans *= x;
+                nn -= 1;
+            }
+        } 
+        if(n < 0) ans = 1.0 / ans;
+        return ans;
     }
 };
