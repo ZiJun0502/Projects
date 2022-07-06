071a234ae78af6041cddc1b3cbf60c6088a37e59
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-07-06 10:49:06 +0800

    Time: 2 ms (64.32%), Space: 5.9 MB (41.61%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int fib(int n) {
+        if(n <= 1) return n;
+        int a0 = 0;
+        int a1 = 1;
+        int ans = 0;
+        for(int i = 0 ; i < n-1 ; i++)
+        {
+            ans = a0 + a1;
+            a0 = a1;
+            a1 = ans;
+        }
+        return ans;
+    }
+};
