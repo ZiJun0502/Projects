453922a18e72e940b4da9e4b27f4a6cfa7ce2ada
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-07-07 21:15:43 +0800

    Time: 2 ms (86.87%), Space: 6.3 MB (79.41%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -3,9 +3,8 @@ public:
     bool isInterleave(string s1, string s2, string s3) {
         int n = s1.size();
         int m = s2.size();
-        //if(n+m < s3.size()) return false;
         bool dp[n+1][m+1];
-        dp[0][0] = (n+m)==s3.size();
+        dp[0][0] = n + m == s3.size();
         for(int i = 1 ; i <= n ; i++) dp[i][0] = dp[i-1][0] && (s1[i-1] == s3[i-1]);
         for(int i = 1 ; i <= m ; i++) dp[0][i] = dp[0][i-1] && (s2[i-1] == s3[i-1]);
         for(int i = 1 ; i <= n ; i++)
