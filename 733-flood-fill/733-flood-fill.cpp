ccc37d8d07bbb0260fdeb9699013dc23847871ce
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-07-08 06:09:21 +0800

    Time: 14 ms (58.96%), Space: 14 MB (79.64%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -8,26 +8,18 @@ public:
         if(visited[i][j]) return;
         visited[i][j] = 1;
         //cout << i <<' ' << j << ' '<<'\n';
-        if(i < n-1 && image[i+1][j] == now)
-        {
+        if(i < n && image[i+1][j] == now)
             DFS(i + 1, j, image);
-        }
         if(i > 0 && image[i-1][j] == now)
-        {
             DFS(i-1, j, image);
-        }
         if(j > 0 && image[i][j-1] == now)
-        {
             DFS(i, j-1, image);
-        }
-        if(j < m-1 && image[i][j+1] == now)
-        {
+        if(j < m && image[i][j+1] == now)
             DFS(i, j+1, image);
-        }
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int i, int j, int color) {
-        n = image.size();
-        m = image[0].size();
+        n = image.size()-1;
+        m = image[0].size()-1;
         c = color;
         now = image[i][j];
         //cout << "hi";
