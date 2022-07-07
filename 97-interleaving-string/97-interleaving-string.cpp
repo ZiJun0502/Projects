ceff882d835b768f2030c402a0761e6a07a949e3
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-07-07 21:13:36 +0800

    Time: 3 ms (85.12%), Space: 6.3 MB (79.41%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -6,23 +6,16 @@ public:
         if(n+m < s3.size()) return false;
         bool dp[n+1][m+1];
         dp[0][0] = 1;
-        for(int i = 1 ; i <= n ; i++) dp[i][0] = dp[i-1][0] && s1[i-1] == s3[i-1];
-        for(int i = 1 ; i <= m ; i++) dp[0][i] = dp[0][i-1] && s2[i-1] == s3[i-1];
+        for(int i = 1 ; i <= n ; i++) dp[i][0] = dp[i-1][0] && (s1[i-1] == s3[i-1]);
+        for(int i = 1 ; i <= m ; i++) dp[0][i] = dp[0][i-1] && (s2[i-1] == s3[i-1]);
         for(int i = 1 ; i <= n ; i++)
         {
             for(int j = 1 ; j <= m ; j++)
             {
-                dp[i][j] = dp[i-1][j] && s3[i+j-1] == s1[i-1]
-                    || dp[i][j-1] && s3[i+j-1] == s2[j-1];
+                dp[i][j] = (dp[i-1][j] && (s3[i+j-1] == s1[i-1]))
+                    || (dp[i][j-1] && (s3[i+j-1] == s2[j-1]));
             }
         }
-        // for(int i = 0 ; i <= n ; i++)
-        // {
-        //     for(int j = 0 ; j <= m ; j++)
-        //     {
-        //         cout << dp[i][j] << ' ';
-        //     }cout<<'\n';
-        // }
         return dp[n][m];
     }
 };
