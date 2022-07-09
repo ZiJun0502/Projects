14202699f809ccf5c435759c0a290ff3559d99fc
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-07-10 01:19:37 +0800

    Time: 440 ms (22.78%), Space: 90.7 MB (44.04%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.cpp
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -1,19 +1,22 @@
+#define pii pair<int, int>
 class Solution {
 public:
-    int maxResult(vector<int>& nums, int k) {
-        int n = nums.size();
-        deque<pair<int,int>> dp;
-        dp.emplace_front(nums[0], 0);
-        for(int i = 1 ; i < n ; i++){
-            while(dp.front().second + k < i){
-                dp.pop_front();
-            }
-            int score = dp.front().first + nums[i];
-            while(!dp.empty() && score >= dp.back().first ){
-                dp.pop_back();
-            }
-            dp.emplace_back(score, i);
+    int maxResult(vector<int>& nums, int k)
+    {
+        int n=nums.size();
+        int score[n];
+        priority_queue<pii> pq;
+        
+        for(int i=n-1 ; i>=0 ; i--)
+        {
+            while(pq.size() && pq.top().second>i+k)
+                pq.pop();
+            
+            score[i]=nums[i];
+            score[i]+=(pq.size() ? pq.top().first : 0);
+            pq.push({score[i], i});
         }
-        return dp.back().first;
+        
+        return score[0];
     }
 };
