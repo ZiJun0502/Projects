50f936f6dff5c044a294eaf2f57ab0de39fe6274
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-07-13 22:57:27 +0800

    Time: 283 ms (7.33%), Space: 93.4 MB (52.30%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -5,12 +5,8 @@ public:
         int n = prices.size();
         int ans = 0;
         for(int i = 1 ; i < n ; i++){
-            if(prices[i] > mini){
-                ans = max(ans, prices[i] - mini);
-            }
-            else{
-                mini = prices[i];
-            }
+            ans = max(ans, prices[i] - mini);
+            mini = min(mini, prices[i]);
         }
         return ans;
     }
