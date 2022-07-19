7cf6317f069a4447d21fe215e717da2bd41e2107
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-07-20 04:18:16 +0800

    Time: 9 ms (99.57%), Space: 9.3 MB (42.46%) - LeetHub

--- 1525-number-of-good-ways-to-split-a-string/1525-number-of-good-ways-to-split-a-string.cpp
+++ 1525-number-of-good-ways-to-split-a-string/1525-number-of-good-ways-to-split-a-string.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int numSplits(string s) {
+        //initialize
         bitset<26> l;
         bitset<26> r;
         
@@ -11,6 +12,7 @@ public:
         memset(right, 0, 4*n);
         left[0] = right[n-1] = 1;
         l[s[0]-'a'] = 1, r[s[n-1] - 'a'] = 1;
+        //loop from left to right.
         for(int i = 1 ; i < n ; i++){
             left[i] = left[i-1];
             if(l[s[i] - 'a'] == 0){
@@ -18,13 +20,15 @@ public:
                 l[s[i] -'a'] = 1;
             }
         }
+        //loop from right to left.
         for(int i = n-2 ; i > -1 ; i--){
             right[i] = right[i+1];
             if(r[s[i] - 'a'] == 0){
-                right[i]++;
+                right[i]+=1;
                 r[s[i] -'a'] = 1;
             }
         }
+        //check good points.
         int ans = 0;
         for(int i = 0 ; i < n-1 ;i++){
             if(left[i] == right[i+1]){
