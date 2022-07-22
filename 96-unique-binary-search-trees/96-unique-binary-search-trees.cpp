9efc2805a47fd23602c4c8ab85c886f103823c02
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-07-22 18:35:55 +0800

    Time: 0 ms (100.00%), Space: 5.9 MB (88.46%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int numTrees(int n) {
         int dp[n+1];
-        memset(dp, 0, 4*(n+1));
+        memset(dp, 0, sizeof(int)*(n+1));
         dp[0] = 1;
         dp[1] = 1;
         for(int i = 2 ; i <= n ; i++){
