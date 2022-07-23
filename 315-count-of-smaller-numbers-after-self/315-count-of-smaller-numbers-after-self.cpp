a1766b12cf95f84e6e87615ce7002bae07237e44
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-07-23 14:55:43 +0800

    Time: 143 ms (99.81%), Space: 70 MB (98.73%) - LeetHub

--- 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
@@ -1,79 +1,21 @@
+template <int n, typename i_t = int>
+struct bit_t {
+    array<i_t, 1 + n> D{};
+	void add(int i, int d = 1) { for (++i; i <= n; i += i & -i) D[i] += d; }
+	i_t sum(int i) const { i_t s = 0; for (++i; i > 0; i -= i & -i) s += D[i]; return s; }
+};
+
 class Solution {
 public:
-    void merge(vector<pair<int, int>> &nums, int start, int mid, int end, vector<int> &res)
-    {
-        vector<pair<int, int>> sorted(end - start + 1);
-
-        int i = start;
-        int j = mid + 1;
-        int k = 0;
-
-        while (i <= mid and j <= end)
-        {
-            if (nums[i].first <= nums[j].first)
-            {
-                sorted[k] = nums[j];
-                j++;
-                k++;
-            }
-            else
-            {
-                res[nums[i].second] += end - j + 1;
-                sorted[k] = nums[i];
-                i++;
-                k++;
-            }
-        }
-
-        while (i <= mid)
-        {
-            sorted[k] = nums[i];
-            i++;
-            k++;
-        }
-
-        while (j <= end)
-        {
-            sorted[k] = nums[j];
-            j++;
-            k++;
-        }
-
-        i = start;
-        k = 0;
-        while (i <= end)
-        {
-            nums[i] = sorted[k];
-            i++;
-            k++;
-        }
-    }
-
-    void Merge_Sort(vector<pair<int, int>> &nums, int start, int end, vector<int> &res)
-    {
-
-        if (start < end)
-        {
-            int mid = start + (end - start) / 2;
-
-            Merge_Sort(nums, start, mid, res);
-            Merge_Sort(nums, mid + 1, end, res);
-            merge(nums, start, mid, end, res);
-        }
-        else
-            return;
-    }
-
-    vector<int> countSmaller(vector<int> &nums)
-    {
-        vector<pair<int, int>> nums2(nums.size());
-        for (int i = 0; i < nums.size(); i++)
-        {
-            nums2[i] = {nums[i], i};
-        }
-        vector<int> res(nums.size(), 0);
-        Merge_Sort(nums2, 0, nums.size() - 1, res);
-
-        return res;
+    vector<int> countSmaller(vector<int>& X) {
+		const int M = 10'000;
+		vector<int> R(X.size());
+		bit_t<2 * M> B;
+        for (int i = X.size(); i--;) {
+			int x = X[i] + M;
+			R[i] = B.sum(x - 1);
+			B.add(x);
+		}
+        return R;
     }
 };
