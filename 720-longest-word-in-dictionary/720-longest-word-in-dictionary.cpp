046ed3ae4c5eb928821371e3de5b1354884da812
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-07-25 02:25:01 +0800

    Time: 86 ms (51.76%), Space: 18.7 MB (85.12%) - LeetHub

--- 720-longest-word-in-dictionary/720-longest-word-in-dictionary.cpp
+++ 720-longest-word-in-dictionary/720-longest-word-in-dictionary.cpp
@@ -5,16 +5,10 @@ public:
         int ans = 0;
         string s;
         sort(words.begin(), words.end());
+        m.insert("");
         for(string word : words){
             int n = word.size();
-            if(n == 1){
-                m.insert(word);
-                if(n > ans){
-                    ans = n;
-                    s = word;
-                }else if(n == ans && word < s) s = word;
-            }
-            else if(m.find(word.substr(0, n-1)) != m.end()){
+            if(m.find(word.substr(0, n-1)) != m.end()){
                 if(n > ans){
                     ans = n;
                     s = word;
