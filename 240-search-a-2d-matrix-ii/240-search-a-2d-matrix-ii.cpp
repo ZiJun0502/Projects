4333168ccaf3da35c177a5ae0b72468c457bef4e
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-07-25 01:15:30 +0800

    Time: 166 ms (54.08%), Space: 14.8 MB (88.55%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
@@ -5,8 +5,9 @@ public:
         int m = matrix[0].size();
         int i = n-1, j = 0;
         while(i >= 0 && j < m){
-            if(matrix[i][j] == target) return true;
-            else if(matrix[i][j] > target) i--;
+            
+            if(matrix[i][j] > target) i--;
+            else if(matrix[i][j] == target) return true;
             else j++;
         }
         return false;
