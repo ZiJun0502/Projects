d16296eb389bd90c8c42bcc1547ff1b8d96307c6
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-07-27 05:34:07 +0800

    Time: 8 ms (44.22%), Space: 11.9 MB (93.07%) - LeetHub

---
+++ 31-next-permutation/31-next-permutation.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1) return;
+        int l = n-1, r = n-1;
+        for(l; l > 0 ; l--){
+            if(nums[l] > nums[l-1]){
+                l--;
+                break;
+            }
+        }
+        if(l == 0 && !(nums[l+1] > nums[l])) reverse(nums.begin(), nums.end());
+        else{
+            for(r ; r > l ; r--){
+                if(nums[r] > nums[l]){
+                    break;
+                }
+            }
+            swap(nums[l], nums[r]);
+            reverse(nums.begin()+l+1, nums.end());
+        }
+        
+    }// 1 2 5 3 4 6
+     // 1 2 5 3 6 4
+     // 1 2 5 4 3 6
+     // 1 2 5 4 6 3
+     // 1 2 5 6 3 4
+     // 1 2 7 6 8 4 3
+     // 1 2 6 3 4 5
+};
