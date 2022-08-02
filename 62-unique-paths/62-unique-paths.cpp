7865afdcc5f88ab10dccaf26b03e293cde97bc53
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-08-02 10:17:47 +0800

    Time: 3 ms (44.87%), Space: 5.8 MB (90.97%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-       int mod = 2e9;
         long long ans = 1;
         int temp = min(m, n) -1;
         int j = m+n-2;
         for(int i = 1 ; i <= temp ; i++){
-            ans = ans * j-- / i % mod;
+            ans = ans * j-- / i;
         }
         return ans;
     }
