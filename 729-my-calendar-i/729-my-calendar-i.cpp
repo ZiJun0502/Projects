084c4aaae498da69ca4ea19f30f09fa10b88bf2c
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-08-03 14:57:02 +0800

    Time: 91 ms (96.92%), Space: 38.8 MB (48.99%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.cpp
+++ 729-my-calendar-i/729-my-calendar-i.cpp
@@ -1,41 +1,16 @@
 class MyCalendar {
 public:
-    vector<int> starts;
-    vector<int> finish;
+    map<int, int> m;
     MyCalendar() {
         
     }
     
     bool book(int start, int end) {
-        if(starts.empty()){
-            starts.push_back(start);
-            finish.push_back(end);
-            return true;
-        }
-        auto i = upper_bound(starts.begin(), starts.end(), start);
-        //cout <<start<<' '<< i - starts.begin()<<'\n'; 
-        
-        if(i == starts.end()){
-            int last = finish[(i-starts.begin())-1];
-            //cout << last <<'\n';
-            if(last > start) return false;
-            finish.insert(finish.begin()+(i-starts.begin()),end);
-            starts.insert(i, start);
-        }
-        else {
-            if(end > *i) return false;
-            if(i != starts.begin()){
-                int last = finish[i-starts.begin()-1];
-                if(last > start) return false;
-            }
-            finish.insert(finish.begin()+(i-starts.begin()),end);
-            starts.insert(i, start);
-            
-        }
-        // for(int i : starts) cout << i <<' ';
-        // cout <<'\n';
+        auto i = m.lower_bound(start);
+        if(i != m.end() && end > i->first) return false;
+        if(i != m.begin() && (--i)->second > start) return false;
+        m[start] = end;
         return true;
-        
     }
 };
 
