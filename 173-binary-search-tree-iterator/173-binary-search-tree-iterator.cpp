d445274b0138e96f1a82547f8419c66551dbac5f
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-08-04 17:15:49 +0800

    Time: 52 ms (37.30%), Space: 24 MB (87.58%) - LeetHub

--- 173-binary-search-tree-iterator/173-binary-search-tree-iterator.cpp
+++ 173-binary-search-tree-iterator/173-binary-search-tree-iterator.cpp
@@ -1,40 +1,36 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class BSTIterator {
+private:
+    stack<TreeNode*> st;
 public:
-    vector<int> vec;
-    int i = 0;
-    void inorder(TreeNode* root){
-        if(root->left) inorder(root->left);
-        vec.push_back(root->val);
-        if(root->right) inorder(root->right);
+    BSTIterator(TreeNode *root) {
+        find_left(root);
     }
-    BSTIterator(TreeNode* root) {
-        inorder(root);
-    }
-    
-    int next() {
-        //cout << i <<'\n';
-        return vec[i++];
-    }
-    
+
+    /** @return whether we have a next smallest number */
     bool hasNext() {
-        return i < vec.size();
+        if (st.empty())
+            return false;
+        return true;
+    }
+
+    /** @return the next smallest number */
+    int next() {
+        TreeNode* top = st.top();
+        st.pop();
+        if (top->right != NULL)
+            find_left(top->right);
+            
+        return top->val;
+    }
+    
+    /** put all the left child() of root */
+    void find_left(TreeNode* root)
+    {
+        TreeNode* p = root;
+        while (p != NULL)
+        {
+            st.push(p);
+            p = p->left;
+        }
     }
 };
-
-/**
- * Your BSTIterator object will be instantiated and called as such:
- * BSTIterator* obj = new BSTIterator(root);
- * int param_1 = obj->next();
- * bool param_2 = obj->hasNext();
- */
