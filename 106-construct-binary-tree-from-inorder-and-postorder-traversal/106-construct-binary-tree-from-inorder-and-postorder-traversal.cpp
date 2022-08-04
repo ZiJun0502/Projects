72d07feb94dc2583fbf44eae8b824962cf0e21c6
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-08-04 15:57:05 +0800

    Time: 45 ms (33.17%), Space: 25.4 MB (99.81%) - LeetHub

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -11,7 +11,8 @@
  */
 class Solution {
 public:
-    void re(vector<int> in, vector<int> po, int inl, int inr, int prel, int prer,
+    vector<int> in, po;
+    void re(int inl, int inr, int prel, int prer,
             TreeNode* now){
         int i;
         if(inl >= inr) return;
@@ -23,18 +24,19 @@ public:
         //cout << i <<'\n';
         if(i-inl > 0){
             now->left = new TreeNode(po[prel + i - inl - 1]);
-            re(in, po, inl, i-1, prel, prel+i-1-inl, now->left);
+            re(inl, i-1, prel, prel+i-1-inl, now->left);
         }else now->left = NULL;
         if(inr-i > 0){
             now->right = new TreeNode(po[prer-1]);
-            re(in, po, i+1, inr, prel+i-inl, prer-1, now->right);
+            re(i+1, inr, prel+i-inl, prer-1, now->right);
         }else now->right = NULL;
         
     }
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
         int n = postorder.size();
+        in = inorder, po = postorder;
         TreeNode* root = new TreeNode(postorder[postorder.size()-1]);
-        re(inorder, postorder, 0, n-1, 0, n-1, root);
+        re(0, n-1, 0, n-1, root);
         return root;
     }
 };
