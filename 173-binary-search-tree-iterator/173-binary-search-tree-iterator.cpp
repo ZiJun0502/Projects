8f0265fb452d34108d8e1dc375233931b4f86dd2
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-08-04 17:16:18 +0800

    Time: 33 ms (85.44%), Space: 24.3 MB (8.62%) - LeetHub

--- 173-binary-search-tree-iterator/173-binary-search-tree-iterator.cpp
+++ 173-binary-search-tree-iterator/173-binary-search-tree-iterator.cpp
@@ -1,36 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class BSTIterator {
-private:
-    stack<TreeNode*> st;
 public:
-    BSTIterator(TreeNode *root) {
-        find_left(root);
+    vector<int> arr;
+    int i = 0;
+    void inorder(TreeNode* root){
+        if(root->left) inorder(root->left);
+        arr.push_back(root->val);
+        if(root->right) inorder(root->right);
     }
-
-    /** @return whether we have a next smallest number */
-    bool hasNext() {
-        if (st.empty())
-            return false;
-        return true;
-    }
-
-    /** @return the next smallest number */
-    int next() {
-        TreeNode* top = st.top();
-        st.pop();
-        if (top->right != NULL)
-            find_left(top->right);
-            
-        return top->val;
+    BSTIterator(TreeNode* root) {
+        inorder(root);
     }
     
-    /** put all the left child() of root */
-    void find_left(TreeNode* root)
-    {
-        TreeNode* p = root;
-        while (p != NULL)
-        {
-            st.push(p);
-            p = p->left;
-        }
+    int next() {
+        //cout << i <<'\n';
+        return arr[i++];
+    }
+    
+    bool hasNext() {
+        return i < arr.size();
     }
 };
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator* obj = new BSTIterator(root);
+ * int param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
+ */
