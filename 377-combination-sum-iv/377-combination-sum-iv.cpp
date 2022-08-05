3ac533f7a9b0d351e92382ddad843090cfe4be70
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-08-05 23:53:20 +0800

    Time: 0 ms (100.00%), Space: 6.6 MB (65.95%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     int combinationSum4(vector<int>& nums, int target) {
-        if(target == 999) return 1;
+        //if(target == 999) return 1;
         int n = nums.size();
         int dp[target+1];
+        int maxi = INT_MAX;
         //memset(dp, 0, 4*(target+1));
         for(int &i : dp) i = 0;
         dp[0] = 1;
         for(int j = 1 ; j <= target ; j++){
             for(int i = 0 ; i < n ; i++){
-                if(nums[i] <= j){
+                if(nums[i] <= j && dp[j] < maxi - dp[j-nums[i]]){
                     dp[j] += dp[j-nums[i]];
                 }
             }
