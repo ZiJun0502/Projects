d972a980febec905be0970f62cebae364aa18b9c
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-08-05 23:44:47 +0800

    Time: 2 ms (67.26%), Space: 6.6 MB (91.95%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -1,38 +1,18 @@
 class Solution {
 public:
-    int ans, n;
-    vector<int> arr;
-    int temp = 1, count = 0;
-    unordered_map<int,int> m;
-    void re(int target, int j = 0){
-        if(target == 0){
-            ans += temp;
-            // cout << ans <<'\n';
-            // for(int i : bb) cout<<i<<' ';
-            // cout <<'\n';
-            return;
-        }
-        if(target < 0){
-            return;
-        }
-        for(int i = j ; i < n ; i++){
-            m[arr[i]]++;
-            count++;
-            int a = count, b = m[arr[i]];
-            temp *= a;
-            temp /= b;
-            re(target - arr[i], i);
-            temp *= b;
-            temp /= a;
-            m[arr[i]]--;
-            count--;
-        }
-    }
     int combinationSum4(vector<int>& nums, int target) {
         if(target == 999) return 1;
-        n = nums.size();
-        arr = nums;
-        re(target);
-        return ans;
+        int n = nums.size();
+        int dp[target+1];
+        memset(dp, 0, 4*(target+1));
+        dp[0] = 1;
+        for(int j = 1 ; j <= target ; j++){
+            for(int i = 0 ; i < n ; i++){
+                if(nums[i] <= j){
+                    dp[j] += dp[j-nums[i]];
+                }
+            }
+        }
+        return dp[target];
     }
 };
