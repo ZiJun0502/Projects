56f540b56cfd6dd7158e0478f63ff73ee6eb5277
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-08-06 17:01:27 +0800

    Time: 46 ms (72.29%), Space: 10.7 MB (77.63%) - LeetHub

---
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int n = s.size(), m = t.size();
+        int dp[n+1][m+1];
+        int maxi = 2147483647;
+        memset(dp, 0, sizeof(dp));
+        for(int i = 0 ; i <= n ; i++) dp[i][0] = 1;
+        for(int i = 1 ; i <= m ; i++) dp[0][i] = 0;
+        for(int j = 1 ; j <= m ; j++){
+            for(int i = 1 ; i <= n ; i++){
+                if(s[i-1] == t[j-1]){
+                    if(maxi - dp[i-1][j] > dp[i-1][j-1])
+                    dp[i][j] = dp[i-1][j] + dp[i-1][j-1];
+                }else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        
+        return dp[n][m];
+    }
+};
