6a5f92a695faf31111d28062829659345caaf3aa
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-08-10 17:44:25 +0800

    Time: 10 ms (46.42%), Space: 8 MB (46.14%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -3,13 +3,7 @@ public:
     vector<int> countBits(int n) {
         vector<int> ans(n+1);
         for(int i = 0 ; i <= n ; i++){
-            int num = i;
-            while(num){
-                if(num % 2 == 1){
-                    ans[i]++;
-                }
-                num/=2;
-            }
+            ans[i] = __builtin_popcount(i);
         }
         return ans;
     }
