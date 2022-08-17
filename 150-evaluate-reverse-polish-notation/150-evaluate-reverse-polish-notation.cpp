c4d6a3a39d43c711fa4bd2a8656ef7a022b9a950
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-08-17 21:17:14 +0800

    Time: 19 ms (43.27%), Space: 11.8 MB (98.42%) - LeetHub

---
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        int n = tokens.size();
+        auto isoper=[](char c){
+            return c == '/' || c == '+' || c =='-' || c == '*';
+        };
+        int stack[10000];
+        int size = 0;
+        for(int i = 0 ; i < n ; i++){
+            // for(int i = 0 ; i < size ; i++) cout << stack[i] <<' ';
+            // cout << '\n';
+            if(tokens[i].size() == 1 && isoper(tokens[i][0])){
+                switch(tokens[i][0]){
+                    case'+':
+                        stack[size-2] = stack[size-1] + stack[size-2];
+                        size--;
+                        break;
+                    case'-':
+                        stack[size-2] = stack[size-2] - stack[size-1];
+                        size--;
+                        break;
+                    case'*':
+                        stack[size-2] = stack[size-1] * stack[size-2];
+                        size--;
+                        break;
+                    case'/':
+                        stack[size-2] = stack[size-2] / stack[size-1];
+                        size--;
+                        break;
+                }
+            }else{
+                stack[size++] = stoi(tokens[i]);
+            }
+        }
+        return stack[0];
+    }
+};
