919b98f1dbcedf9b435982a35d51870c87458bed
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-08-25 15:12:03 +0800

    Time: 53 ms (50.82%), Space: 9.9 MB (43.44%) - LeetHub

---
+++ 936-stamping-the-sequence/936-stamping-the-sequence.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<int> movesToStamp(string stamp, string target) {
+        vector<int> ans;
+        int count = 0, n = target.size(), m = stamp.size();
+        auto check = [&](int start, int len){
+            for(int i = 0 ; i < len ; i++){
+                if(target[i + start] != stamp[i] && target[i + start] != '?') return 0;
+            }
+            return 1;
+        };
+        auto Stamp = [&](int start, int len){
+            for(int i = 0 ; i < len ; i++){
+                if(target[i + start] != '?'){
+                    target[i + start] = '?';
+                    count ++;
+                }
+                
+            }
+        };
+        bool temp;
+        int round = 0;
+        while(count != n){
+            temp = 0;
+            for(int i = 0 ; i < n ; i++){
+                if(check(i, m)){
+                    Stamp(i, m);
+                    ans.push_back(i);
+                    temp = 1;
+                }
+            }
+            if(!temp || ans.size() > 10 * n) return {};
+        }
+        reverse(begin(ans), end(ans));
+        return ans;
+    }
+};
