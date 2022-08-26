3abf1c3416c13e4fc04a98733f12e98a147fbab6
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-08-27 04:38:25 +0800

    Time: 3 ms (78.14%), Space: 7.2 MB (28.14%) - LeetHub

--- 842-split-array-into-fibonacci-sequence/842-split-array-into-fibonacci-sequence.cpp
+++ 842-split-array-into-fibonacci-sequence/842-split-array-into-fibonacci-sequence.cpp
@@ -4,28 +4,27 @@ public:
     int n;
     vector<int> temp;
     vector<int> ans;
-    void re(string s, int j, int last, int llast){
+    bool re(string s, int j, int last, int llast){
         int val = 0;
-        //cout << j << '\n';
-        
         if(j == n && ans.size() > 2){
-            temp = ans;
+            return true;
         }
-        if(INT_MAX - last < llast) return;
+        if(INT_MAX - last < llast) return 0;
         for(int i = j ; i < n ; i++){
             if(val >= MAX) break;
             val = val * 10 + (s[i] - '0');
             if(last + llast == val || ans.size() < 2){
                 ans.push_back(val);
-                re(s, i+1, val, last);
+                if(re(s, i+1, val, last)) return true;
                 ans.pop_back();
             }
             if(val == 0) break;
         }
+        return false;
     }
     vector<int> splitIntoFibonacci(string num) {
         n = num.size();
         re(num, 0, 0, 0);
-        return temp;
+        return ans;
     }
 };
