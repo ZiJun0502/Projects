2602a94ada941877dc9235aa984819839742d2ac
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-08-27 04:39:06 +0800

    Time: 3 ms (78.14%), Space: 7.1 MB (47.63%) - LeetHub

--- 842-split-array-into-fibonacci-sequence/842-split-array-into-fibonacci-sequence.cpp
+++ 842-split-array-into-fibonacci-sequence/842-split-array-into-fibonacci-sequence.cpp
@@ -2,12 +2,11 @@ class Solution {
 public:
     #define MAX 214748365
     int n;
-    vector<int> temp;
     vector<int> ans;
     bool re(string s, int j, int last, int llast){
         int val = 0;
         if(j == n && ans.size() > 2){
-            return true;
+            return 1;
         }
         if(INT_MAX - last < llast) return 0;
         for(int i = j ; i < n ; i++){
@@ -15,12 +14,12 @@ public:
             val = val * 10 + (s[i] - '0');
             if(last + llast == val || ans.size() < 2){
                 ans.push_back(val);
-                if(re(s, i+1, val, last)) return true;
+                if(re(s, i+1, val, last)) return 1;
                 ans.pop_back();
             }
             if(val == 0) break;
         }
-        return false;
+        return 0;
     }
     vector<int> splitIntoFibonacci(string num) {
         n = num.size();
