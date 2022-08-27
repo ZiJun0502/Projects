c748f42a2cb62fae37d3d21b4a63300e61248375
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-08-27 12:22:44 +0800

    Time: 7 ms (89.98%), Space: 10.7 MB (100.00%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -1,27 +1,12 @@
 class Solution {
 public:
     vector<int> frequencySort(vector<int>& nums) {
-        short int count[201] = {};
-        for(int i : nums){
-            count[i+100]++;
-        }
-        vector<int> m[101];
-        for(int i = 200 ; i >= 0 ; i--){
-            if(count[i]){
-                //cout << count[i]<<' '<<i-100 <<'\n';
-                m[count[i]].push_back(i-100);
-            }
-        }
-        vector<int> ans;
-        int ind = 0;
-        for(int i = 0 ; i <= 100 ; i++){
-            if(m[i].size() > 0){
-                for(int j : m[i]){
-                    auto k = vector<int>(i, j);
-                    ans.insert(ans.end(), k.begin(), k.end());
-                }
-            }
-        }
-        return ans;
+        int cnt[201] = {};
+        for (auto n : nums)
+            ++cnt[n + 100];
+        sort(begin(nums), end(nums), [&](int a, int b) {
+           return cnt[a + 100] == cnt[b + 100] ? a > b : cnt[a + 100] < cnt[b + 100];
+        });
+        return nums;
     }
 };
