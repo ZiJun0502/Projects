663d486f7c109300f519f9ac5199131362cf6eff
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-08-27 11:58:05 +0800

    Time: 11 ms (90.15%), Space: 8.8 MB (20.35%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
@@ -1,22 +1,31 @@
 class Solution {
 public:
+    int find(char c){
+        if(c >= 'a' && c <= 'z') return c-'a';
+        else if(c >= 'A' && c <= 'Z') return c-'A'+26;
+        else return c-'0'+52;
+    }
+    int findd(int i){
+        if(i >= 0 && i < 26) return i+'a';
+        else if(i >= 26 && i < 52) return i-26+'A';
+        else return i+'0'-52;
+    }
     string frequencySort(string s) {
-        unordered_map<char, int> freqOfChar;
-        for(char c : s) {
-            freqOfChar[c] ++;
+        int count[62] = {};
+        for(char c : s){
+            count[find(c)]++;
         }
-        
-        map<int, string> dupStr;
-        for(auto v : freqOfChar) {
-            char c = v.first;
-            int n = v.second;
-            dupStr[n] += string(n, c);
+        priority_queue<pair<int, char>> q;
+        string ans;
+        for(int i = 0 ; i < 62 ; i++){
+            if(count[i])
+                q.push({count[i], findd(i)});
         }
-        
-        string res;
-        for(auto rit = dupStr.rbegin(); rit != dupStr.rend(); ++rit) {
-            res += rit->second;
+        while(!q.empty()){
+            auto i = q.top();
+            q.pop();
+            ans += string(i.first, i.second);
         }
-        return res;
+        return ans;
     }
 };
