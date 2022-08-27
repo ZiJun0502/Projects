fe867eb5f312e9551c17cb25a09d3ab3e27cc58a
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-08-27 12:44:22 +0800

    Time: 95 ms (22.38%), Space: 23.3 MB (14.25%) - LeetHub

--- 554-brick-wall/554-brick-wall.cpp
+++ 554-brick-wall/554-brick-wall.cpp
@@ -1,19 +1,11 @@
 class Solution {
 public:
     int leastBricks(vector<vector<int>>& wall) {
-        int n = 0, maxi = 0;
-        for(int i : wall[0]) n += i;
-        unordered_map<int,int> count;
-        for(auto i : wall){
-            int len = 0;
-            for(int j : i){
-                len += j;
-                if(len != n){
-                    count[len-1]++;
-                    maxi = max(maxi, count[len-1]);
-                }
-            }
-        }
-        return wall.size() - maxi;
+        unordered_map<int, int> edges;
+        auto min_bricks = wall.size();
+        for (auto row : wall)
+            for (auto i = 0, width = 0; i < row.size() - 1; ++i) // skip last brick
+                min_bricks = min(min_bricks, wall.size() - (++edges[width += row[i]]));
+        return min_bricks;
     }
 };
