411e17481c72b8e47678cf76d175e7729aa90fab
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-08-27 11:17:47 +0800

    Time: 134 ms (79.34%), Space: 33.2 MB (42.02%) - LeetHub

--- 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     int minSwaps(string s) {
         int n = s.size();
-        bool stack[n/2];
-        memset(stack, 0, sizeof(stack));
+        bool* stack = new bool[n/2];
         bool* size = stack;
         for(int i = 0 ; i < n ; i++){
             if(s[i] == '['){
