d05d47f10b7501e700d37b2270213ac3c1e66b5b
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-09-01 02:39:55 +0800

    Time: 531 ms (95.39%), Space: 83.8 MB (66.56%) - LeetHub

--- 363-max-sum-of-rectangle-no-larger-than-k/363-max-sum-of-rectangle-no-larger-than-k.cpp
+++ 363-max-sum-of-rectangle-no-larger-than-k/363-max-sum-of-rectangle-no-larger-than-k.cpp
@@ -1,31 +1,35 @@
 class Solution {
 public:
-    int maxSumSubmatrix(vector<vector<int>>& m, int k) {
-        int res = INT_MIN, rows = m.size(), cols = m[0].size();
-        for (int l = 0; l < cols; ++l) {
-            vector<int> sums(rows);
-            for (int r = l; r < cols; ++r) {
-                int kadane = 0, max_kadane = INT_MIN;
-                for (int i = 0; i < rows; ++i) {
-                    sums[i] += m[i][r];
-                    kadane = max(kadane + sums[i], sums[i]);
-                    max_kadane = max(max_kadane, kadane);
+    int maxSumSubmatrix(vector<vector<int>>& matrix, int maxS) {
+        int maxA = INT32_MIN, r = matrix.size(), c = matrix[0].size();
+        for(int i = 0; i < c; ++i) {
+            vector<int> sum(r, 0);
+            if(maxA == maxS) return maxA; // Stop if we can reach maxS
+            for(int j = i; j < c; ++j) {
+                for(int k = 0; k < r; ++k) sum[k] += matrix[k][j];
+                
+                //First try Kadane's Algo and see if maxSum is less than maxS. 
+                int curMax = INT32_MIN, curSum = 0;
+                for(int k = 0; k < r; ++k) {
+                    curSum += sum[k];
+                    curMax = max(curMax, curSum);
+                    if(curSum < 0) curSum = 0;
                 }
-                if (max_kadane <= k) {
-                    res = max(res, max_kadane);
-                    continue;
-                }
-                set<int> s = {0};
-                int run_sum = 0;
-                for (int sum : sums) {
-                    run_sum += sum;
-                    auto it = s.lower_bound(run_sum - k);
-                    if (it != end(s))
-                        res = max(res, run_sum - *it);
-                    s.insert(run_sum);
+                if(curMax <= maxS) {maxA = max(maxA, curMax); continue;}
+                
+                // Only apply slow method when there maxSum that is greater than maxS.
+                int csum = 0;
+                set<int> s({csum});
+                for(int k = 0; k < r; ++k) {
+                    csum += sum[k];
+                    auto it = s.lower_bound(csum - maxS);
+                    if(it != s.end()) maxA = max(maxA, csum - *it);
+                    s.insert(csum);
                 }
             }
         }
-        return res;
-    }  
+        return maxA;
+    }
 };
+
+auto gucciGang = []() {std::ios::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);return 0;}();
