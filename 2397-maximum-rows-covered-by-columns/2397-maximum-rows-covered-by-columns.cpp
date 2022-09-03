5088cf05a309a66c31a8b0850b2dcc9bf9a425df
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-09-04 00:33:29 +0800

    Time: 25 ms (100.00%), Space: 9.2 MB (100.00%) - LeetHub

--- 2397-maximum-rows-covered-by-columns/2397-maximum-rows-covered-by-columns.cpp
+++ 2397-maximum-rows-covered-by-columns/2397-maximum-rows-covered-by-columns.cpp
@@ -1,34 +1,37 @@
 class Solution {
 public:
-    int maximumRows(vector<vector<int>>& mat, int cols) {
-        int m=mat.size(),n=mat[0].size(),ans=0;
-        int mask=((1<<n)-1);
-        // use mask to generate every possible columns combination
-        while(mask){
-            unordered_set<int>select;
-            for(int i=0;i<n;i++){
-                if(((mask>>i)&1)==1)
-                    select.insert(i);
-            }
-            // if subset contains exaclty "cols" columns then we can use this subset
-            if(select.size()==cols){
-                int cnt=0;
-                for(int j=0;j<m;j++){
-                    bool flag=true;
-                    for(int k=0;k<n;k++){
-                        // if any row contains 1 in  column other than columns which we have in "select" then this row is not valid row for our answer
-                        if(mat[j][k]==1 && select.count(k)==0){
-                            flag=false;
-                            break;
-                        }
+    int n, m, tar = 0;
+    int temp = 0, ans = 0;
+    unordered_set<int> reco;
+    vector<vector<int>> arr;
+    void re(int now = 0){
+        if(reco.size() == tar){
+            int temp = n;
+            //for(int i : reco)cout << i <<' ';
+            //cout << '\n';
+            for(int i = 0 ; i < n ; i++){
+                for(int j = 0 ; j < m ; j++){
+                    if(reco.find(j) != reco.end()) continue;
+                    if(arr[i][j] == 1){
+                        temp--;
+                        break;
                     }
-                    if(flag)
-                        cnt++;
                 }
-                ans=max(ans,cnt);
             }
-            mask--;
+            ans = max(ans, temp);
+            return;
         }
+        for(int i = now ; i < m-(tar - reco.size())+1 ; i++){
+            reco.insert(i);
+            re(i+1);
+            reco.erase(i);
+        }
+    }
+    int maximumRows(vector<vector<int>>& mat, int cols) {
+        n = mat.size(), m = mat[0].size();
+        tar = cols;
+        arr = mat;
+        re();
         return ans;
     }
 };
