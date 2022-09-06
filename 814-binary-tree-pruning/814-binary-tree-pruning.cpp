e3aef4d8aab25f8e0cd78049ecea3d28e7192a93
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-09-06 14:32:32 +0800

    Time: 9 ms (9.94%), Space: 8.8 MB (64.29%) - LeetHub

--- 814-binary-tree-pruning/814-binary-tree-pruning.cpp
+++ 814-binary-tree-pruning/814-binary-tree-pruning.cpp
@@ -15,8 +15,7 @@ public:
         if(root){
             root->left = pruneTree(root->left);
             root->right = pruneTree(root->right);
-            if(!root->left && !root->right && root->val==0)
-                return NULL;
+            if(!root->left && !root->right && root->val==0) return NULL;
         }
         return root;
     }
