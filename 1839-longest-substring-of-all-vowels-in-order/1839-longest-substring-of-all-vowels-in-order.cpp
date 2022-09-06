2268ee57f0d01e3e95d6a6b890c77901c9b8c041
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-09-06 16:22:08 +0800

    Time: 95 ms (93.24%), Space: 26.9 MB (60.83%) - LeetHub

--- 1839-longest-substring-of-all-vowels-in-order/1839-longest-substring-of-all-vowels-in-order.cpp
+++ 1839-longest-substring-of-all-vowels-in-order/1839-longest-substring-of-all-vowels-in-order.cpp
@@ -3,7 +3,7 @@ public:
     int longestBeautifulSubstring(string word) {
     int n = word.size();
     int ans = 0, j = 0;
-    for(int i = 0 ; i < n-4 ; i++) {
+    for(int i = 0 ; i < n-4 ; ++i) {
         if (word[i] == 'a') {
             int count = 0;
             for(j = i+1 ; j < n && word[j] >= word[j-1]; j++){
