e72071faf614afe56947a9e13c6a86764fdaea8c
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-09-06 14:50:25 +0800

    Time: 24 ms (43.52%), Space: 12.5 MB (90.98%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<vector<int>> groupThePeople(vector<int>& group) {
         int n = group.size();
-        vector<int> count[n+1];
+        unordered_map<int, vector<int>> count;
         vector<vector<int>> ans;
         for(int i = 0 ; i < n ; i++){
             count[group[i]].push_back(i);
