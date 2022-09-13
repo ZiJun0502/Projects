b5d6e201a49efb7410b342086b7a8d1ba2bf6a05
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-09-13 22:39:44 +0800

    Time: 4 ms (33.48%), Space: 5.9 MB (60.02%) - LeetHub

--- 2224-minimum-number-of-operations-to-convert-time/2224-minimum-number-of-operations-to-convert-time.cpp
+++ 2224-minimum-number-of-operations-to-convert-time/2224-minimum-number-of-operations-to-convert-time.cpp
@@ -15,15 +15,14 @@ public:
         return ans;
     }
     int convertTime(string cur, string cor) {
-        int n = 5, ans = 0, h1, h0, m1, m0;
+        int ans = 0, h1, h0, m1, m0;
         m1 = ((cor[3]-'0') * 10) + cor[4]-'0', m0 = ((cur[3]-'0') * 10) + cur[4]-'0';
         h1 = ((cor[0]-'0') * 10) + cor[1]-'0', h0 = ((cur[0]-'0') * 10) + cur[1]-'0';
         if (h0 > h1) {
-            int ans = (24 - h0) + h1 + cal(m1 + (60 - m0));
-            if (m0 > m1) return ans - 1;
-            else return ans;
+            ans = (24 - h0) + h1 + cal(m1 + (60 - m0));
+            return m0 > m1 ? ans-1 : ans;
         }
         if (m0 > m1) return h1 - h0 - 1 + cal(m1 + (60 - m0));
-        else return h1 - h0 + cal(m1 - m0);
+        return h1 - h0 + cal(m1 - m0);
     }
 };
