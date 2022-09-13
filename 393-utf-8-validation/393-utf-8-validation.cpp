eb9edf58503aa546e514ee6dcdb653ebc673bc77
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-09-13 20:42:47 +0800

    Time: 64 ms (6.23%), Space: 13.9 MB (76.31%) - LeetHub

---
+++ 393-utf-8-validation/393-utf-8-validation.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool validUtf8(vector<int>& data) {
+        int n = data.size();
+        bitset<8> b;
+        for(int i = 0 ; i < n ; i++){
+            b = data[i];
+            cout << b << '\n';
+            if(b[7] == 1){
+                int count = 0;
+                for(int j = 6 ; j >= 0 ; j--){
+                    if(b[j] == 1) count++;
+                    else break;
+                }
+                if(count > 3) return false;
+                if(count == 0) return false;
+                while(count-- && i+1 < n){
+                    b = data[++i];
+                    //cout << b << '\n';
+                    //cout << b[b.size()-1] << b[b.size()-2] <<'\n';
+                    if(b[b.size()-1] != 1 || b[b.size()-2] != 0) 
+                        return false;
+                }
+                //cout << count;
+                if(count != -1) return false;
+            }
+            else continue;
+        }
+        return true;
+    }
+};
