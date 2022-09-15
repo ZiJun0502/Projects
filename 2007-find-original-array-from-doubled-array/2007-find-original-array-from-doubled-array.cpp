04cfcf012f739c666e59d29f8520e25af00f7bb4
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-09-15 17:03:21 +0800

    Time: 290 ms (98.57%), Space: 118.1 MB (96.70%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
@@ -5,7 +5,7 @@ public:
         sort(begin(changed), end(changed), greater<int>());
         vector<int> ans;
         int count = changed.size();
-        if(count % 2) return {};
+        //if(count % 2) return {};
         while(count > 0 && changed[count-1] == 0){
             count--;
             m[0]++;
@@ -17,7 +17,7 @@ public:
         for(int i : changed){
             //cout << i << ' ';
             m[i]++;
-            if(i < 50001 && i != 0 && m[i*2]>0 || i == 0 && m[i] > 1){
+            if(i < 50001 && i != 0 && m[i*2]>0){
                 ans.push_back(i);
                 m[i]--, m[i*2]--;
                 count -= 2;
