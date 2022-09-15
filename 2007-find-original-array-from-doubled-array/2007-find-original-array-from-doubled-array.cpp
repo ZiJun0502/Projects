759d7a7d567e36270dc4ba6e35ce159f043f1b51
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-09-15 16:54:23 +0800

    Time: 578 ms (72.06%), Space: 118.6 MB (96.15%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     vector<int> findOriginalArray(vector<int>& changed) {
-        int m[200001] = {};
+        int m[100001] = {};
         sort(begin(changed), end(changed), greater<int>());
         vector<int> ans;
         int count = changed.size();
         for(int i : changed){
             //cout << i << ' ';
             m[i]++;
-            if(i != 0 && m[i*2]>0 || i == 0 && m[i] > 1){
+            if(i <= 50000 && i != 0 && m[i*2]>0 || i == 0 && m[i] > 1){
                 ans.push_back(i);
                 m[i]--, m[i*2]--;
                 count -= 2;
