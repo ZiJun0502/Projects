96c50b84743b64cd89c97c2c6cc803bc8b8000c6
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-09-16 13:00:34 +0800

    Time: 593 ms (67.83%), Space: 57 MB (97.20%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> nums, mul;
-    int dp[1000][1000];
+    int dp[1001][1001];
     int n, m;
     int re(int now, int l, int r){
         if(now == m) return 0;
@@ -15,8 +15,8 @@ public:
     int maximumScore(vector<int>& _nums, vector<int>& _mul) {
         nums = _nums, mul = _mul;
         n = nums.size(), m = mul.size();
-        for(int i = 0 ; i < 1000 ; i++){
-            for(int j = 0 ; j < 1000; j++) dp[i][j] = -2;
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j < m; j++) dp[i][j] = -2;
         }
         return re(0, 0, n-1);
     }
