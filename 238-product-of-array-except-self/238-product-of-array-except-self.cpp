98bd40b9b2f7d15770d2c65bafc3328932108792
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-09-19 11:08:15 +0800

    Time: 70 ms (5.24%), Space: 24 MB (87.86%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -5,10 +5,8 @@ public:
         vector<int> ans(n, 1);
         int l = 1, r = 1;
         for(int i = 0 ; i < n ; i++){
-            ans[i] *= l;
-            ans[n-i-1] *= r;
-            l *= nums[i];
-            r *= nums[n-i-1];
+            ans[i] *= l,ans[n-i-1] *= r;
+            l *= nums[i],r *= nums[n-i-1];
         }
         return ans;
     }
