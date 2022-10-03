239230cbb6fb4c9cb5aa214e873861152a0a2839
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-10-03 16:29:45 +0800

    Time: 462 ms (9.49%), Space: 95.5 MB (50.05%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
@@ -3,7 +3,6 @@ public:
     int minCost(string colors, vector<int>& neededTime) {
         int ans = 0, n = colors.size(), maxi;
         for(int i = 0 ; i < n-1 ; i++){
-            
             if(colors[i] == colors[i+1]){
                 maxi = neededTime[i];
                 while(i < n-1 && colors[i] == colors[i+1]){
@@ -11,8 +10,7 @@ public:
                     ans += neededTime[i];
                     i++;
                 }
-                ans += neededTime[i];
-                ans -= maxi;
+                ans += neededTime[i] - maxi;
             }
             
         }
