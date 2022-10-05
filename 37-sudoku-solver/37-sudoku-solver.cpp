a25759877130282afd082a84d5e6c3041b23f7ac
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-10-05 23:57:54 +0800

    Time: 4 ms (98.58%), Space: 6.6 MB (16.43%) - LeetHub

--- 37-sudoku-solver/37-sudoku-solver.cpp
+++ 37-sudoku-solver/37-sudoku-solver.cpp
@@ -1,47 +1,79 @@
 class Solution {
 public:
-    int n;
-    vector<vector<char>>* arr;
-    bool valid(int i, int j, int k)
-    {
-        for(int ii = 0 ; ii < n ; ii++)
-            if((*arr)[ii][j]-'0' == k) return false;
-        for(int jj = 0 ; jj < n ; jj++)
-            if((*arr)[i][jj]-'0' == k) return false;
-        int istart = i - i%3;
-        int jstart = j - j%3;
-        for(int ii = istart ; ii < istart+3 ; ii++)
-            for(int jj = jstart ; jj < jstart + 3 ; jj++)
-                if((*arr)[ii][jj]-'0' == k) return false;
-        return true;
-    }
-    bool help(int i, int j)
-    {
-        if(j == 9)
-        {
-            i++;
-            j = 0;
+    #define vi vector<int>
+    #define vvi vector<vi>
+    #define For(i, n) for(int i = 0 ; i < n ; i++)
+    #define FOR(i, n , m) for(int i = n ; i < m ; i++)
+    #define reset(i) memset(i, 0, sizeof(i));
+    int lg(int n){
+        switch (n){
+            case 1:return 1;
+            case 2:return 2;
+            case 4:return 3;
+            case 8:return 4;
+            case 16:return 5;
+            case 32:return 6;
+            case 64:return 7;
+            case 128:return 8;
+            case 256:return 9;
+            case 512:return 0;
         }
-        if(i == 9) return true;
-        if((*arr)[i][j] =='.')
-        {
-            //cout << i << ' ' << j << '\n';
-            for(int k = 1 ; k < 10 ; k++)
-            {
-                if(valid(i, j, k))
-                {
-                    (*arr)[i][j] = k + '0';
-                    if(help(i, j+1) == true) return true;
-                    (*arr)[i][j] = '.';
+        return -1;
+    }
+    int col[9];
+    int row[9];
+    int s_grid[3][3];
+    int grid[9][9];
+    const int mask = 511;
+    int lowbit(int n){
+        return (n & -n);
+    }
+    bool find(int i, int j, vector<vector<char>>& board){
+        if(i == 9){
+            return 1;
+        }
+        if(grid[i][j] != 0){
+            if(find(j==8?i+1:i, j==8?0:j+1, board)){
+                return 1;
+            }
+        }else{
+            int nums = (mask & ~(col[j] | row[i] | s_grid[i/3][j/3]));
+            for(int num = 0 ; nums != 0 ;){
+                num = lowbit(nums);
+                nums ^= num;
+                grid[i][j] = num;
+                col[j] ^= num;
+                row[i] ^= num;
+                s_grid[i/3][j/3] ^= num;
+                board[i][j] = lg(num) + '0';
+                if(find(j==8?i+1:i, j==8?0:j+1, board)){
+                    return 1;
                 }
+                grid[i][j] = 0;
+                col[j] ^= num;
+                row[i] ^= num;
+                board[i][j] = '.';
+                s_grid[i/3][j/3] ^= num;
             }
         }
-        else return help(i, j+1);
-        return false;
+        return 0;
     }
+    void solve(vector<vector<char>>& board){
+
+        For(i, 9){
+            For(j,  9){
+                if(isdigit(board[i][j])){
+                    grid[i][j] = 1 << (board[i][j] - '1');
+                    col[j]^= grid[i][j];
+                    row[i]^= grid[i][j];
+                    s_grid[i/3][j/3] ^= grid[i][j];
+                }else grid[i][j] = 0;
+            }
+        }
+        find(0, 0, board);
+    }
+
     void solveSudoku(vector<vector<char>>& board) {
-        n = board.size();
-        arr = &board;
-        help(0,0);
+        solve(board);
     }
 };
