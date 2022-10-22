bbaddd24e20fd5eeff6c847af68cf935c3278438
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-10-22 22:25:02 +0800

    Time: 11 ms (92.73%), Space: 7.6 MB (84.25%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        if (s.size() == 0 || t.size() == 0) return "";
-        vector<int> remaining(128, 0);
-        int required = t.size();
-        for (int i = 0; i < required; i++) remaining[t[i]]++;
-        // left is the start index of the min-length substring ever found
-        int min = INT_MAX, start = 0, left = 0, i = 0;
-        while(i <= s.size() && start < s.size()) {
-            if(required) {
-                if (i == s.size()) break;
-                remaining[s[i]]--;
-                if (remaining[s[i]] >= 0) required--;
+        int n = s.size(), m = t.size();
+        int T[128] = {};
+        int count = t.size();
+        for (int i = 0; i < count; i++) 
+            T[t[i]]++;
+        int mini = 1e5+5, last = 0, left = 0, i = 0;
+        while(i <= s.size() && last < s.size()) {
+            if(count) {
+                if (i == s.size()) 
+                    break;
+                T[s[i]]--;
+                if (T[s[i]] >= 0) 
+                    count--;
                 i++;
             } else {
-                if (i - start < min) {
-                    min = i -start;
-                    left = start;
+                if (i - last < mini) {
+                    mini = i -last;
+                    left = last;
                 }
-                remaining[s[start]]++;
-                if (remaining[s[start]] > 0) required++;
-                start++;
+                T[s[last]]++;
+                if (T[s[last]] > 0) count++;
+                last++;
             }
         }
-        return min == INT_MAX? "" : s.substr(left, min);
+        return mini == 1e5+5? "" : s.substr(left, mini);
     }
 };
