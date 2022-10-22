81c24d2bffbf9ba22b585a250ab3a3ca57f8a20b
ZiJun0502 <96115937+ZiJun0502@users.noreply.github.com>
2022-10-22 22:20:05 +0800

    Time: 8 ms (95.68%), Space: 7.8 MB (48.70%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,54 +1,28 @@
 class Solution {
 public:
-    unordered_map<char, int> T;
-    bool check(unordered_map<char,int>& record){
-        for(auto& i : T){
-            if(record.find(i.first) == record.end()) return 0;
-            if(i.second > record[i.first]){
-                return 0;
-            }
-        }
-        return 1;
-    }
-        
     string minWindow(string s, string t) {
-        int ans = 1e5+5;
-        string ss;
-        unordered_map<char,int> record;
-        int n = s.size(), m = t.size();
-        for(char c : t){
-            T[c]++;
-        }
-        int l = -1;
-        int pos[n];
-        memset(pos, 0, sizeof(pos));
-        int size = 0;
-        int now = 0;
-        for(int i = 0 ; i < n ; i++){
-            record[s[i]]++;
-            if(T.find(s[i]) != T.end()){
-                pos[size++] = i;
-                if(l == -1) l = i;
-            }
-            if(check(record)){
-                if(i-l < ans){
-                    ans = i - l;
-                    ss = string(s.begin()+l, s.begin()+i+1);
+        if (s.size() == 0 || t.size() == 0) return "";
+        vector<int> remaining(128, 0);
+        int required = t.size();
+        for (int i = 0; i < required; i++) remaining[t[i]]++;
+        // left is the start index of the min-length substring ever found
+        int min = INT_MAX, start = 0, left = 0, i = 0;
+        while(i <= s.size() && start < s.size()) {
+            if(required) {
+                if (i == s.size()) break;
+                remaining[s[i]]--;
+                if (remaining[s[i]] >= 0) required--;
+                i++;
+            } else {
+                if (i - start < min) {
+                    min = i -start;
+                    left = start;
                 }
-                while(1){
-                    int last = pos[now++];
-                    record[s[last]]--;
-                    if(now < size){
-                        l = pos[now];
-                    }
-                    if(T[s[last]] > record[s[last]]) break;
-                    if(i-l < ans){
-                        ans = i - l;
-                        ss = string(s.begin()+l, s.begin()+i+1);
-                    }
-                }
+                remaining[s[start]]++;
+                if (remaining[s[start]] > 0) required++;
+                start++;
             }
         }
-        return ss;
+        return min == INT_MAX? "" : s.substr(left, min);
     }
 };
